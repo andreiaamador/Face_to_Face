77786fa5d57ab218778ae27d1f309d2211dd2ed1
HHTSoul <31953872+HHTSoul@users.noreply.github.com>
2017-10-19 12:52:06 +0100

    teste

---
+++ Face_to_Face/Face_to_Face/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Face_to_Face.Startup))]
+namespace Face_to_Face
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
