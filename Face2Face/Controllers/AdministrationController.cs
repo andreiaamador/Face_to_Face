fb5a60276e9b8f6911f88eadf5c46b85f31e5322
SaraLopes <31954669+SaraLopes@users.noreply.github.com>
2017-10-26 14:24:34 +0000

    .

---
+++ Face2Face/Controllers/AdministrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Face2Face.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdministrationController : Controller
+    {
+        // GET: Administration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Eliminar perfis
+        // Gestao Homepage (inclui reporte de abusos)
+        [Authorize(Roles = "Users")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // Pesquisar Eventos()
+        // Criar/editar/apagar eventos
+        // Aderir eventos
+        //Editar/eliminar perfil
+        //Reportar abuso/classificar
+
+    }
+}
