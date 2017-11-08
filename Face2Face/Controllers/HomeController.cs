230c1902ce5ca8363cc797b8744aec1aebff3081
SaraLopes <31954669+SaraLopes@users.noreply.github.com>
2017-11-08 17:00:29 +0000

    contacts

--- Face2Face/Controllers/HomeController.cs
+++ Face2Face/Controllers/HomeController.cs
@@ -1,18 +1,23 @@
-﻿using Face2Face.Models;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using System.Xml.Linq;
+using Face2Face.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity;
+using System.Web.Security;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace Face2Face.Controllers
 {
     public class HomeController : Controller
     {
+        private Face2FaceEntities1 db = new Face2FaceEntities1();
         public ActionResult Index()
         {
 
@@ -20,23 +25,23 @@ namespace Face2Face.Controllers
             {
                 return View();
             }
-            else {
+            else
+            {
                 return RedirectToAction("EventsList", "EventTables");
             }
-            
+
         }
 
         public ActionResult About()
         {
-           
+
             return View();
         }
 
         public ActionResult Contact()
         {
             //ViewBag.Message = "Your contact page.";
-
             return View();
-        }  
+        }
     }
 }
